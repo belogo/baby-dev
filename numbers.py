f436451d03cce73616ab7908d8dd09f07979050b
belogo <32401077+belogo@users.noreply.github.com>
2022-04-02 20:34:30 +0300

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,31 @@
+# ***
+# * Working with numbers
+#
+
+# init a variable with a number
+my_age = 35
+
+
+# make a list (array) with numbers
+# hard codded
+ages = [10, 9, 12, 32, 50, 22]
+
+
+# find oldest
+oldest = max(ages)
+
+
+# find youngest
+youngest = min(ages)
+
+
+# find sum of all ages
+sum_of_ages = sum(ages)
+
+
+# find count of ages
+count_of_ages = len(age)
+
+
+# find average age in ages
+avg_age = sum(ages) / len(ages)
