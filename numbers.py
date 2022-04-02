b1f4b13f4c949e85a510511dd6338797beabcead
belogo <32401077+belogo@users.noreply.github.com>
2022-04-02 23:39:57 +0300

    Update numbers.py
    Convert tabs to 4 spaces

--- numbers.py
+++ numbers.py
@@ -51,7 +51,7 @@ penultimate = ages[-1]
 ages_histogram = {}
 
 for age in ages:
-	ages_histogram[age] = ages_histogram.get(age, 0) + 1
+    ages_histogram[age] = ages_histogram.get(age, 0) + 1
 
 
 # find the most common age
@@ -68,52 +68,52 @@ unique_ages = list(set(ages))
 
 # make a list with different / unique ages 
 def unique_ages_list(ages):
-	unique = []
-	for age in ages:
-		if age not in unique:
-			unique.append(age)
-	return unique
+    unique = []
+    for age in ages:
+        if age not in unique:
+            unique.append(age)
+    return unique
 
 
 
 class Arr:
-	def __init__(self, size=0, default=None):
-		self.arr = [default] * size
-		self.size = size
+    def __init__(self, size=0, default=None):
+        self.arr = [default] * size
+        self.size = size
 
-	def add(self, value):
-		self.arr.append(value)
-		self.size += 1
-		return self
+    def add(self, value):
+        self.arr.append(value)
+        self.size += 1
+        return self
 
-	def remove(self, rem_value):
-		self.arr = [ v for v in self.arr if v != rem_value]
-		self.size = len(arr)
-		return self
+    def remove(self, rem_value):
+        self.arr = [ v for v in self.arr if v != rem_value]
+        self.size = len(arr)
+        return self
 
-	def set(self, idx, value):
-		if 0 < idx < self.size:
-			self.arr[idx] = value
-			return self
-		else:
-			raise IndexError
+    def set(self, idx, value):
+        if 0 < idx < self.size:
+            self.arr[idx] = value
+            return self
+        else:
+            raise IndexError
 
-	def map(self, func):
-		for idx, elem in enumerate(self.arr):
-			self.arr[idx] = func(elem)
-		return self
-
-
-	def encounter(self, value):
-		return sum( 1 for v in self.arr if v == value)
-
-	def __repr__(self):
-		return repr(self.arr)
+    def map(self, func):
+        for idx, elem in enumerate(self.arr):
+            self.arr[idx] = func(elem)
+        return self
 
 
-	def __str__(self):
-		return str(self.arr)
+    def encounter(self, value):
+        return sum( 1 for v in self.arr if v == value)
+
+    def __repr__(self):
+        return repr(self.arr)
 
 
-	def size(self):
-		return self.size
+    def __str__(self):
+        return str(self.arr)
+
+
+    def size(self):
+        return self.size
